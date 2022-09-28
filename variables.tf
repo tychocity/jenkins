f9c91d51ae60fff729de49aad0867ebc7dfaebe2
tychocity <tychocity@gmail.com>
2022-09-28 14:31:32 -0300

    default ami

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "ami_id" {
 
   default = {
     us-east-1    = "ami-035b3c7efe6d061d5"
+    us-east-2    = "ami-0f924dc71d44d23e2"
     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
     eu-central-1 = "ami-9787h5h6nsn"
   }
